e2425e12ed398e31d888012d62909c01a2c92d03
Wenbang Wang <wenbang.wang@gmail.com>
2014-10-15 23:43:43 -0400

    grunt tasks

---
+++ grunt/aliases.js
@@ -0,0 +1,12 @@
+var _ = require('lodash-node/modern/objects');
+
+module.exports = function (grunt) {
+    'use strict';
+
+    var aliases = {};
+
+    _.extend(aliases, require('./aliases-analysis')(grunt));
+    _.extend(aliases, require('./aliases-local')(grunt));
+
+    return aliases;
+};
